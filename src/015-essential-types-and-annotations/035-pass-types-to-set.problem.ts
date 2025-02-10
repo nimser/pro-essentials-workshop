e28600e6bb52cf7a05bc61c1194e401e4597b287
Nicolas Wormser <nicolas@codewithflair.org>
2025-02-10 20:26:56 +0100

    Ex 35

--- src/015-essential-types-and-annotations/035-pass-types-to-set.problem.ts
+++ src/015-essential-types-and-annotations/035-pass-types-to-set.problem.ts
@@ -1,14 +1,14 @@
 // CODE
 
-const userIds = new Set();
+const userIds = new Set<number>()
 
 // TESTS
 
-userIds.add(1);
-userIds.add(2);
-userIds.add(3);
+userIds.add(1)
+userIds.add(2)
+userIds.add(3)
 
 // @ts-expect-error
-userIds.add("123");
+userIds.add("123")
 // @ts-expect-error
-userIds.add({ name: "Max" });
+userIds.add({ name: "Max" })
