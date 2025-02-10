a7a38142d2f25fbd0ab764ca59c8aa91eba5d397
Nicolas Wormser <nicolas@codewithflair.org>
2025-02-10 20:50:36 +0100

    Ex 42

--- src/016.5-ide-superpowers/042-adding-tsdoc-comments-for-hovers.problem.ts
+++ src/016.5-ide-superpowers/042-adding-tsdoc-comments-for-hovers.problem.ts
@@ -1,3 +1,12 @@
+/**
+ * Adds two numbers together.
+ *
+ * @example
+ *
+ * myFunction(1, 2);
+ */
 const myFunction = (a: number, b: number) => {
-  return a + b;
-};
+  return a + b
+}
+
+myFunction(1, 2)
