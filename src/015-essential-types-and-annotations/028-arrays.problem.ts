d7db1f65ec5c84ba891aac5c56d0a42307aea3e0
Nicolas Wormser <nicolas@codewithflair.org>
2025-02-10 20:04:04 +0100

    Ex 28

--- src/015-essential-types-and-annotations/028-arrays.problem.ts
+++ src/015-essential-types-and-annotations/028-arrays.problem.ts
@@ -2,16 +2,17 @@
 
 // How do we type 'items' in the ShoppingCart?
 type ShoppingCart = {
-  userId: string;
-};
+  userId: string
+  items: string[]
+}
 
 // TESTS
 
 const processCart = (cart: ShoppingCart) => {
   // Do something with the cart in here
-};
+}
 
 processCart({
   userId: "user123",
   items: ["item1", "item2", "item3"],
-});
+})
