7cc79e30f5ab72256ea13beebe0ee625d199186b
Nicolas Wormser <nicolas@codewithflair.org>
2025-02-10 19:46:17 +0100

    Exercise 020

--- src/015-essential-types-and-annotations/020-basic-types-with-function-parameters.problem.ts
+++ src/015-essential-types-and-annotations/020-basic-types-with-function-parameters.problem.ts
@@ -1,13 +1,13 @@
-import { Expect, Equal } from "@total-typescript/helpers";
+import { Expect, Equal } from "@total-typescript/helpers"
 
 // CODE
 
-const add = (a: boolean, b: boolean) => {
-  return a + b;
-};
+const add = (a: number, b: number) => {
+  return a + b
+}
 
 // TESTS
 
-const result = add(1, 2);
+const result = add(1, 2)
 
-type test = Expect<Equal<typeof result, number>>;
+type test = Expect<Equal<typeof result, number>>
