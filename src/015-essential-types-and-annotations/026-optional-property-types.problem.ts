625bc273a5df1e1ce7826b8d6ff356e69b3b3655
Nicolas Wormser <nicolas@codewithflair.org>
2025-02-10 20:00:47 +0100

    Ex 26

--- src/015-essential-types-and-annotations/026-optional-property-types.problem.ts
+++ src/015-essential-types-and-annotations/026-optional-property-types.problem.ts
@@ -1,14 +1,14 @@
-import { Expect, Equal } from "@total-typescript/helpers";
-import { expect, it } from "vitest";
+import { Expect, Equal } from "@total-typescript/helpers"
+import { expect, it } from "vitest"
 
 // CODE
 
-const concatName = (user: { first: string; last: string }) => {
+const concatName = (user: { first: string; last?: string }) => {
   if (!user.last) {
-    return user.first;
+    return user.first
   }
-  return `${user.first} ${user.last}`;
-};
+  return `${user.first} ${user.last}`
+}
 
 // TESTS
 
@@ -16,19 +16,19 @@ it("should return the full name", () => {
   const result = concatName({
     first: "John",
     last: "Doe",
-  });
+  })
 
-  type test = Expect<Equal<typeof result, string>>;
+  type test = Expect<Equal<typeof result, string>>
 
-  expect(result).toEqual("John Doe");
-});
+  expect(result).toEqual("John Doe")
+})
 
 it("should only return the first name if last name not provided", () => {
   const result = concatName({
     first: "John",
-  });
+  })
 
-  type test = Expect<Equal<typeof result, string>>;
+  type test = Expect<Equal<typeof result, string>>
 
-  expect(result).toEqual("John");
-});
+  expect(result).toEqual("John")
+})
