cb1d9bb2475514ed9e3eea5fb5519dfc4476463c
Nicolas Wormser <nicolas@codewithflair.org>
2025-02-10 20:18:58 +0100

    Ex 32.5

--- src/015-essential-types-and-annotations/032.5-any.problem.ts
+++ src/015-essential-types-and-annotations/032.5-any.problem.ts
@@ -1,24 +1,24 @@
-import { expect, it } from "vitest";
+import { expect, it } from "vitest"
 
 const handleFormData = (e: any) => {
-  e.preventDefault();
-  const data = new FormData(e.terget);
-  const value = Object.fromEntries(data.entries());
-  return value;
-};
+  e.preventDefault()
+  const data = new FormData(e.target)
+  const value = Object.fromEntries(data.entries())
+  return value
+}
 
 it("Should handle a form submit", () => {
-  const form = document.createElement("form");
+  const form = document.createElement("form")
   form.innerHTML = `
     <input name="name" value="John Doe" />
-  `;
+  `
 
   form.onsubmit = (e) => {
-    const value = handleFormData(e);
-    expect(value).toEqual({ name: "John Doe" });
-  };
+    const value = handleFormData(e)
+    expect(value).toEqual({ name: "John Doe" })
+  }
 
-  form.requestSubmit();
+  form.requestSubmit()
 
-  expect.assertions(1);
-});
+  expect.assertions(1)
+})
