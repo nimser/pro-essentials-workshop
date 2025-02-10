16102a9ac4414b038e5aa6f25af95dc1284651a3
Nicolas Wormser <nicolas@codewithflair.org>
2025-02-10 19:58:48 +0100

    Ex 25

--- src/015-essential-types-and-annotations/025-object-literal-types.problem.ts
+++ src/015-essential-types-and-annotations/025-object-literal-types.problem.ts
@@ -1,17 +1,17 @@
-import { Expect, Equal } from "@total-typescript/helpers";
-import { expect, it } from "vitest";
+import { Expect, Equal } from "@total-typescript/helpers"
+import { expect, it } from "vitest"
 
-const concatName = (user) => {
-  return `${user.first} ${user.last}`;
-};
+const concatName = (user: { first: string; last: string }) => {
+  return `${user.first} ${user.last}`
+}
 
 it("should return the full name", () => {
   const result = concatName({
     first: "John",
     last: "Doe",
-  });
+  })
 
-  type test = Expect<Equal<typeof result, string>>;
+  type test = Expect<Equal<typeof result, string>>
 
-  expect(result).toEqual("John Doe");
-});
+  expect(result).toEqual("John Doe")
+})
