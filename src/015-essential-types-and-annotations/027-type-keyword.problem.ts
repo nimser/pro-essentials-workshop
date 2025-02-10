b6300a0a985f00cda9e28938c1405db7e5024c6c
Nicolas Wormser <nicolas@codewithflair.org>
2025-02-10 20:03:08 +0100

    Ex 27

--- src/015-essential-types-and-annotations/027-type-keyword.problem.ts
+++ src/015-essential-types-and-annotations/027-type-keyword.problem.ts
@@ -1,35 +1,34 @@
-import { Expect, Equal } from "@total-typescript/helpers";
-import { expect, it } from "vitest";
+import { Expect, Equal } from "@total-typescript/helpers"
+import { expect, it } from "vitest"
 
-const getRectangleArea = (rectangle: { width: number; height: number }) => {
-  return rectangle.width * rectangle.height;
-};
+type Rectangle = { width: number; height: number }
 
-const getRectanglePerimeter = (rectangle: {
-  width: number;
-  height: number;
-}) => {
-  return 2 * (rectangle.width + rectangle.height);
-};
+const getRectangleArea = (rectangle: Rectangle) => {
+  return rectangle.width * rectangle.height
+}
+
+const getRectanglePerimeter = (rectangle: Rectangle) => {
+  return 2 * (rectangle.width + rectangle.height)
+}
 
 it("should return the area of a rectangle", () => {
   const result = getRectangleArea({
     width: 10,
     height: 20,
-  });
+  })
 
-  type test = Expect<Equal<typeof result, number>>;
+  type test = Expect<Equal<typeof result, number>>
 
-  expect(result).toEqual(200);
-});
+  expect(result).toEqual(200)
+})
 
 it("should return the perimeter of a rectangle", () => {
   const result = getRectanglePerimeter({
     width: 10,
     height: 20,
-  });
+  })
 
-  type test = Expect<Equal<typeof result, number>>;
+  type test = Expect<Equal<typeof result, number>>
 
-  expect(result).toEqual(60);
-});
+  expect(result).toEqual(60)
+})
