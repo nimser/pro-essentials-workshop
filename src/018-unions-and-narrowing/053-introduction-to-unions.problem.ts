80b46fb9d8b2630d20a040f5eae7468d145e7292
Nicolas Wormser <nicolas@codewithflair.org>
2025-02-11 20:35:00 +0100

    Ex 55

--- src/018-unions-and-narrowing/053-introduction-to-unions.problem.ts
+++ src/018-unions-and-narrowing/053-introduction-to-unions.problem.ts
@@ -1,17 +1,17 @@
-import { Equal, Expect } from "@total-typescript/helpers";
+import { Equal, Expect } from "@total-typescript/helpers"
 
-function getUsername(username: string) {
+function getUsername(username: string | null) {
   if (username !== null) {
-    return `User: ${username}`;
+    return `User: ${username}`
   } else {
-    return "Guest";
+    return "Guest"
   }
 }
 
 // TESTS
 
-const result = getUsername("Alice");
-type test = Expect<Equal<typeof result, string>>;
+const result = getUsername("Alice")
+type test = Expect<Equal<typeof result, string>>
 
-const result2 = getUsername(null);
-type test2 = Expect<Equal<typeof result2, string>>;
+const result2 = getUsername(null)
+type test2 = Expect<Equal<typeof result2, string>>
--- src/018-unions-and-narrowing/054-literal-types.problem.ts
+++ src/018-unions-and-narrowing/054-literal-types.problem.ts
@@ -1,26 +1,26 @@
-function move(direction: string, distance: number) {
+function move(direction: "up" | "left" | "down" | "right", distance: number) {
   // Move the specified distance in the given direction
 }
 
 // TESTS
 
-move("up", 10);
-move("left", 5);
+move("up", 10)
+move("left", 5)
 
 move(
   // @ts-expect-error - "up-right" is not a valid direction
   "up-right",
-  10,
-);
+  10
+)
 
 move(
   // @ts-expect-error - "down-left" is not a valid direction
   "down-left",
-  20,
-);
+  20
+)
 
 move(
   "up",
   // @ts-expect-error - "20" is not a valid distance
-  "20",
-);
+  "20"
+)
