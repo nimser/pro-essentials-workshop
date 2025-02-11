168d861c986a5d7addf7432411acb538d6381fdf
Nicolas Wormser <nicolas@codewithflair.org>
2025-02-11 20:55:41 +0100

    Ex 61

--- src/018-unions-and-narrowing/059-narrowing-with-if-statements.problem.ts
+++ src/018-unions-and-narrowing/059-narrowing-with-if-statements.problem.ts
@@ -1,19 +1,17 @@
-import { expect, it } from "vitest";
+import { expect, it } from "vitest"
 
 function validateUsername(username: string | null): boolean {
   // Rewrite this function to make the error go away
-  return username.length > 5;
-
-  return false;
+  return username ? username.length > 5 : false
 }
 
 it("should return true for valid usernames", () => {
-  expect(validateUsername("Matt1234")).toBe(true);
+  expect(validateUsername("Matt1234")).toBe(true)
 
-  expect(validateUsername("Alice")).toBe(false);
-  expect(validateUsername("Bob")).toBe(false);
-});
+  expect(validateUsername("Alice")).toBe(false)
+  expect(validateUsername("Bob")).toBe(false)
+})
 
 it("Should return false for null", () => {
-  expect(validateUsername(null)).toBe(false);
-});
+  expect(validateUsername(null)).toBe(false)
+})
--- src/018-unions-and-narrowing/061-map-has-doesnt-narrow-map-get.problem.ts
+++ src/018-unions-and-narrowing/061-map-has-doesnt-narrow-map-get.problem.ts
@@ -1,11 +1,12 @@
 type Event = {
-  message: string;
-};
+  message: string
+}
 
 const processUserMap = (eventMap: Map<string, Event>) => {
-  if (eventMap.has("error")) {
-    const message = eventMap.get("error").message;
+  const event = eventMap.get("error")
+  if (event) {
+    const message = event.message
 
-    throw new Error(message);
+    throw new Error(message)
   }
-};
+}
