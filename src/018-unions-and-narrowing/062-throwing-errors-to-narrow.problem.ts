4e1da2b0ade0f8adf897f351828d966ac76e443d
Nicolas Wormser <nicolas@codewithflair.org>
2025-02-11 21:02:09 +0100

    Ex 62

--- src/018-unions-and-narrowing/062-throwing-errors-to-narrow.problem.ts
+++ src/018-unions-and-narrowing/062-throwing-errors-to-narrow.problem.ts
@@ -1,7 +1,8 @@
-import { Equal, Expect } from "@total-typescript/helpers";
+import { Equal, Expect } from "@total-typescript/helpers"
 
-const appElement = document.getElementById("app");
+const appElement = document.getElementById("app")
+if (!appElement) {
+  throw Error("no app element")
+}
 
-// How do I ensure that appElement is defined?
-
-type Test = Expect<Equal<typeof appElement, HTMLElement>>;
+type Test = Expect<Equal<typeof appElement, HTMLElement>>
